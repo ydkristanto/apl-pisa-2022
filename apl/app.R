2c4984827fc4fa41dbea07fbd6ad39a19154495f
ydkristanto <yosepdwikristanto@usd.ac.id>
2024-02-04 21:57:53 +0700

    Add dataset

--- apl/app.R
+++ apl/app.R
@@ -1,21 +1,27 @@
-shinyApp(
-  page_navbar(
-    title = "Conditional sidebar",
-    id = "nav",
-    sidebar = sidebar(
-      conditionalPanel(
-        "input.nav === 'Page 1'",
-        "Page 1 sidebar"
-      ),
-      conditionalPanel(
-        "input.nav === 'Page 2'",
-        "Page 2 sidebar"
-      )
+library(shiny)
+library(tidyverse)
+library(bslib)
+
+ui <- page_navbar(
+  title = "Indonesia dalam PISA 2022",
+  id = "nav",
+  sidebar = sidebar(
+    conditionalPanel(
+      "input.nav === 'Page 1'",
+      "Page 1 sidebar"
     ),
-    nav_panel("Page 1", "Page 1 contents"),
-    nav_panel("Page 2", "Page 2 contents")
+    conditionalPanel(
+      "input.nav === 'Page 2'",
+      "Page 2 sidebar"
+    )
   ),
-  server = function(...) {
-    # no server logic required
-  }
+  nav_panel("Hasil Belajar", "Page 1 contents"),
+  nav_panel("Proses dan Konten Matematika", "Page 2 contents"),
+  nav_panel("Kesetaraan", "Page 3 contents"),
+  nav_panel("Tren Hasil Belajar dan Kesetaraan", "Page 3 contents")
 )
+server <-  function(...) {
+  
+}
+
+shinyApp(ui, server)
